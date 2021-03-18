7ad7b82f0a1f327d545f18383327df16fd8ae4f0
Usama <ua63510@gmail.com>
2021-03-18 17:00:29 +0500

    'new'

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require("gulp");
 const gap = require("gulp-append-prepend");
 
-gulp.task("licenses", async function () {
+async function license() {
   // this is to add Creative Tim licenses in the production mode for the minified js
   gulp
     .src("build/static/js/*chunk.js", { base: "./" })
@@ -9,13 +9,13 @@ gulp.task("licenses", async function () {
       gap.prependText(`/*!
 
 =========================================================
-* Light Bootstrap Dashboard React - v2.0.0
+* Paper Kit React - v1.2.0
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+* Copyright 2020 Creative Tim (http://www.creative-tim.com)
 
-* Coded by Creative Tim
+* Coded by HarshElectronics
 
 =========================================================
 
@@ -32,13 +32,13 @@ gulp.task("licenses", async function () {
       gap.prependText(`<!--
 
 =========================================================
-* Light Bootstrap Dashboard React - v2.0.0
+* Paper Kit React - v1.2.0
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+* Copyright 2020 Creative Tim (http://www.creative-tim.com)
 
-* Coded by Creative Tim
+* Coded by HarshElectronics
 
 =========================================================
 
@@ -55,13 +55,13 @@ gulp.task("licenses", async function () {
       gap.prependText(`/*!
 
 =========================================================
-* Light Bootstrap Dashboard React - v2.0.0
+* Paper Kit React - v1.2.0
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+* Copyright 2020 Creative Tim (http://www.creative-tim.com)
 
-* Coded by Creative Tim
+* Coded by HarshElectronics
 
 =========================================================
 
@@ -71,4 +71,7 @@ gulp.task("licenses", async function () {
     )
     .pipe(gulp.dest("./", { overwrite: true }));
   return;
-});
+}
+
+gulp.task("licenses", license);
+gulp.task("default", license);
