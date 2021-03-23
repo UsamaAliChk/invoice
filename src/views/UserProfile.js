bf65d67fd60ee027b203eb138809a56c01c59d06
Usama <ua63510@gmail.com>
2021-03-23 15:18:04 +0500

    'changed'

--- src/views/UserProfile.js
+++ src/views/UserProfile.js
@@ -288,11 +288,11 @@ function User() {
               <Col md="4">
                 <Row>
                 <div>
-                <img src={invoice} style={{height:"300px",width:"280px",marginLeft:'15px',marginTop:'-120px'}}/>
+                <img src={invoice} style={{height:"300px",width:"310px",marginLeft:'10px',marginTop:'-120px'}}/>
                 </div>
                 </Row>
                 <Row>
-                <Link to={"maps"}><Button style={{marginLeft:"20px",width:"270px",marginTop:'20px'}}>GENERATE INVOICE</Button></Link>
+                <Link to={"maps"}><Button style={{marginLeft:"15px",width:"300px",marginTop:'20px'}}>GENERATE INVOICE</Button></Link>
                 </Row>
               
               </Col>
